1db101e2f3fbd68621b3e13b5e1d3ac6c9e9a700
psg9790 <81291990+psg9790@users.noreply.github.com>
2023-03-07 17:16:28 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 2.22 ms, Memory: 5.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    vector<int> mem(26, -1);
+    for(int i =0; i< s.length(); i++){
+        int idx = s[i] - 'a';
+        if(mem[idx] == -1)
+        {
+            answer.push_back(-1);
+        }
+        else
+        {
+            answer.push_back(i - mem[idx]);
+        }
+        mem[idx] = i;
+    }
+    return answer;
+}
