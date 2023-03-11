e7f498c13e7f59f330d82affba633bf0b3802a57
psg9790 <81291990+psg9790@users.noreply.github.com>
2023-03-11 22:03:31 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
@@ -8,10 +8,13 @@ int solution(string t, string p) {
     int answer = 0;
     int plen = p.length();
     int tlen = t.length();
+    int pval = stoi(p);
     int isize = tlen - plen;
     for(int i = 0; i <= isize; i++) {
         string _str = t.substr(i, plen);
-        if(_str <= p) {
+        cout << _str << '\n';
+        int token = stoi(_str);
+        if(token <= pval) {
             answer++;
         }
     }
