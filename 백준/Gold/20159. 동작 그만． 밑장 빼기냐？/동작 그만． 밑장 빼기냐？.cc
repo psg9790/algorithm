4818a2cda407bfcade79036f72773ca7d3dd26ec
psg9790 <81291990+psg9790@users.noreply.github.com>
2023-08-22 11:43:02 +0900

    [Gold V] Title: 동작 그만. 밑장 빼기냐?, Time: 8 ms, Memory: 3200 KB -BaekjoonHub

--- 백준/Gold/20159. 동작 그만． 밑장 빼기냐？/동작 그만． 밑장 빼기냐？.cc
+++ 백준/Gold/20159. 동작 그만． 밑장 빼기냐？/동작 그만． 밑장 빼기냐？.cc
@@ -14,38 +14,28 @@ int n;
 vector<int> v;
 vector<int> odd;
 vector<int> even;
-// vector<int> dp;
 void solve()
 {
     cin >> n;
     v.resize(n + 2);
     odd.resize(n + 2, 0);
     even.resize(n + 2, 0);
-    // dp.resize(n);
     for (int i = 2; i < n + 2; i++)
     {
         cin >> v[i];
         if (i % 2 == 0)
-        {
             even[i] += even[i - 2] + v[i];
-        }
         else
-        {
             odd[i] += odd[i - 2] + v[i];
-        }
     }
     int maxv = even[n];
     debug(maxv);
     for (int i = 2; i < n + 2; i++)
     {
         if (i % 2 == 0)
-        {
             maxv = max(maxv, even[i - 2] + odd[odd.size() - 1] - odd[i - 1]);
-        }
         else
-        {
             maxv = max(maxv, even[i - 1] + odd[odd.size() - 3] - odd[i - 2]);
-        }
     }
     cout << maxv << '\n';
 }
