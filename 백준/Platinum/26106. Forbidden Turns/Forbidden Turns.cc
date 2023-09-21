4016cb26345ff7f548f259377af79a3b4efc54b5
psg9790 <81291990+psg9790@users.noreply.github.com>
2023-09-21 19:23:54 +0900

    [Platinum V] Title: Forbidden Turns, Time: 1736 ms, Memory: 91428 KB -BaekjoonHub

---
+++ 백준/Platinum/26106. Forbidden Turns/Forbidden Turns.cc
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ip = pair<int, int>;
+using ld = long double;
+using lp = pair<ll, ll>;
+
+#define all(v) (v).begin(), (v).end()
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+
+#ifndef ONLINE_JUDGE
+#include "C:\DEBUG.h"
+#else
+#define debug(x...)
+#endif
+
+int n, m, k;
+int src, dst;
+vector<vector<ip>> adj;
+set<tuple<int, int, int>> forbid;
+set<string> visited;
+ll ans = 0;
+
+class route
+{
+public:
+    int num;
+    ll cost;
+    ip past;
+
+    string Serialize()
+    {
+        string rst;
+        rst += to_string(num);
+        rst += ',';
+        // rst += to_string(cost);
+        // rst += ',';
+        rst += to_string(past.first);
+        rst += ',';
+        rst += to_string(past.second);
+        debug(rst);
+        return rst;
+    }
+
+    vector<route *> Simulate()
+    {
+        vector<route *> rst;
+        for (auto next : adj[num])
+        {
+            route *rr = new route();
+            rr->num = next.first;
+            rr->cost = cost + next.second;
+            rr->past = {num, past.first};
+            rst.push_back(rr);
+        }
+        return rst;
+    }
+
+    tuple<int, int, int> Past3()
+    {
+        return {num, past.first, past.second};
+    }
+};
+struct routeCmp
+{
+    bool operator()(route *a, route *b)
+    {
+        return a->cost > b->cost;
+    }
+};
+
+void Dijkstra()
+{
+    priority_queue<route *, vector<route *>, routeCmp> pq;
+
+    route *init = new route();
+    init->num = src;
+    init->cost = 0;
+    init->past = {-1, -1};
+    pq.push(init);
+
+    while (!pq.empty())
+    {
+        route *frt = pq.top();
+        pq.pop();
+
+        if (frt->num == dst)
+        {
+            ans = frt->cost;
+            return;
+        }
+
+        vector<route *> ret = frt->Simulate();
+        for (auto r : ret)
+        {
+            string serial = r->Serialize();
+            if (visited.find(serial) != visited.end()) // 중복케이스
+                continue;
+
+            tuple<int, int, int> t = r->Past3();
+            if (forbid.find(t) != forbid.end()) // 금지 루트
+                continue;
+
+            visited.insert(serial);
+            pq.push(r);
+        }
+    }
+
+    ans = -1;
+}
+
+void solve()
+{
+    cin >> n >> m >> k;
+    cin >> src >> dst;
+
+    adj.resize(m + 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        int ss, ee, cc;
+        cin >> ss >> ee >> cc;
+        adj[ss].push_back({ee, cc});
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+        int r1, r2, r3;
+        cin >> r1 >> r2 >> r3;
+        forbid.insert({r1, r2, r3});
+        forbid.insert({r3, r2, r1});
+    }
+
+    Dijkstra();
+    cout << ans << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+}
